f9d1dd806afd6e0bc7e77c528980eb88b8e71618
Martin <ogradymartin7@gmail.com>
2015-05-11 11:32:26 +0100

    Added cloudinary basic setup which needs an enviromental variable that contains the config params

--- config/express.js
+++ config/express.js
@@ -1,10 +1,11 @@
-var express = require("express");
-var session      = require('express-session');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var flash    = require('connect-flash');
-var multer   = require('multer');
-var methodOverride = require('method-override');
+var express 	 	 = require("express");
+var session      	 = require('express-session');
+var cookieParser 	 = require('cookie-parser');
+var bodyParser 	 	 = require('body-parser');
+var flash    	 	 = require('connect-flash');
+var cloudinary 	 	 = require('cloudinary');
+var multer   	     = require('multer');
+var methodOverride   = require('method-override');
 var expressValidator = require('express-validator');
 
 /**
@@ -18,12 +19,23 @@ module.exports = function (app, passport) {
 	app.use(cookieParser()); // read cookies (needed for auth)
 
 	//============================
-	// Body Parser Configuration
+	// MIddleware Configuration
 	//=============================
 	app.use(bodyParser.urlencoded({ extended: true })); //configure app to use bodyParser()
 	app.use(bodyParser.json()); //this will let us get the data from a POST
 	app.use(multer({dest: __dirname + '../../app/public/uploads'}));
 	app.use(expressValidator());
+	//=====================
+	// Cloudinary Config
+	//=====================
+	if (typeof(process.env.CLOUDINARY_URL)=='undefined'){
+		console.warn('!! cloudinary config is undefined !!');
+		console.warn('export CLOUDINARY_URL or set dotenv file')
+	}else{
+		console.log('cloudinary config:');
+		console.log(cloudinary.config())
+	}
+	
 	//======================
 	// View Configuration
 	//=======================
