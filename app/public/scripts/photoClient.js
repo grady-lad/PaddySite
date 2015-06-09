4262cf8fdf67b008c31eb8def36be719eaa41f50
Martin <ogradymartin7@gmail.com>
2015-06-09 23:35:03 +0200

    Cleaned up photoClient
    Stopped appending to the DOM sooo much, hyped! Had to use a counter and
    create a new row every time a moduls of 3 was successful. Seems to load
    a lot quick now. TODO: make render method into a seperate method. Also
    follow javasript playground tutorials.

--- app/public/scripts/photoClient.js
+++ app/public/scripts/photoClient.js
@@ -1,99 +1,61 @@
-//Defining the photo model
-var photoModel = Backbone.Model.extend({
-	defaults: {
-		idattribute: '_id'
-	}
-
+var photo = Backbone.Model.extend({
+idAtrribute: "id",
 });
-//Defining the illustration gallery collection
-// this collection will be returned when it hits /api/gallery
+
 var photoCollection = Backbone.Collection.extend({
-	
-	url: '/api/gallery',
-	model: photoModel
-});
-//This will be used for the single illustration page
-var singlePhotoCollection = Backbone.Collection.extend({
-	
-	url: '/api/singlePhoto',
-	model: photoModel
-});
-//This view is used to create a new column with an image for every illustration
-// that is within the gallery
+model: photo,
+url: "/api/gallery"
+
+  });
+
 var photoView = Backbone.View.extend({
-	
-	setupCol: function(){
-		//Handlebar template
-		var template = $("#illustrationTemplate").html();
-		var compiled = Handlebars.compile(template);
-		//Create the col div and append the image and link
-		var col = $('<div class = "col-md-4 gallery-image"></div>');
-		var aRef = $('<a href="photo?id=' + this.model.image.created_at + '"/>');
-		aRef.prepend('<img class="img-responsive" height="300" width="300" src ="' + this.model.image.url + '"/>');
-		col.prepend(aRef);
-		return col;
-	},
-	
-	render: function(){
-		//Create a col var, add it to the page element then return
-		var col = this.setupCol();
-		this.$el.html(col);
-		return this;
+
+
+render: function(){
+    var template = $("#illustrationTemplate").html();
+    var compiled = Handlebars.compile(template);
+    var html = compiled(this.model.attributes);
+    this.$el.html(html);
+    return this;
+
    }
-});
+ });
 
-var photoCollectionView = Backbone.View.extend({
-	//When instansisated this method is used.
-	//We wait for the collection to be reset and we call on setuprow
-	initialize: function(){
-    this.listenTo(this.collection, "reset", this.setupRow);
- 	},
- 
- 	render: function(row){
- 		//Appending row to the element
- 		this.$el.append(row);		
- 		return this;
-  },
-  
-  setupRow: function(){
-	//Transfrom rowCollection to a JSON object
-	var rowCollection = this.collection.toJSON();
-	//Check to see if Row exisits TODO ERROR CHECKING
-	if(this.collection){
-		for (var j = 0; j < this.collection.length; j++){
-			//Used to create a new row every 3 images
-			if (j == 0 || j % 3 == 0){
-				var row = $('<div class="row"></div>');
-				for (var i = j; i < (3 + j); i++ ){
-					if (i >= this.collection.length) break;
-					//Create the row column
-					var photoV = new photoView({model: rowCollection[i]});
-					row.prepend(photoV.render().el);
-					this.render(row);
-				}
-			}
-		}
-	}
+ var photoCollectionView = Backbone.View.extend({
+ initialize: function(){
+    this.listenTo(this.collection, "reset", this.render);
+ },
+ render: function(){
+    this.$el.html("");
+    var counter = "0";
+    var row;
+    this.collection.each(function(photo){
+    	console.log("counter is " + counter);
+    	if (counter == 0 || counter % 3 == 0){
+    		console.log("counter here is " + counter);
+    		console.log("moduls");
+    		row = $('<div class="row"></div>');
+    	}
+    	var photoV = new photoView({model: photo});
+    	row.prepend(photoV.render().el);
+    	this.$el.append(row);
+    	counter++;
+    }, this);
+    return this;
   }
+ });
 
-});
-
-var photoRouter = Backbone.Router.extend({
-	//Organising what methods to use when a certain url is hit
-	routes: {
-		// URL: Method
-		"gallery": "illustrationGallery",
-		"photo:id": "singleIllustration"
-	},
-	illustrationGallery: function(){
-		var collection = new photoCollection();
-	    collection.fetch({reset: true});
-	    var view = new photoCollectionView({collection: collection});
-	    $(".app").html(view.render().el); 
-	},
-	
-	singleIllustration: function(id){
-		console.log("heeey");
-	}
-	
+ var photoRouter = Backbone.Router.extend({
+ routes: {
+ "gallery": "illustration"
+ },
+ illustration: function() {
+    var collection = new photoCollection();
+    collection.fetch({reset: true});
+    //IF I ADD THIS ALERT THE COLLECTION IS NOW DEFINED?
+    //alert(collection);
+    console.log(collection);
+    var view = new photoCollectionView({collection: collection});
+    $(".app").html(view.render().el);   
+ }
 });
