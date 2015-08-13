ccf97048cef5555121922b9a40bf2e2ee4a4e072
Martin <ogradymartin7@gmail.com>
2015-08-13 11:03:07 +0200

    adding promises to asynchronus the backbone view

--- app/public/scripts/views/photoCollection.js
+++ app/public/scripts/views/photoCollection.js
@@ -42,25 +42,34 @@ define(["backbone", "views/photo"], function(Backbone, photoView) {
 	 		"use strict";
 	 		var counter = 0;
 	 		var row;
-	 		this.collection.each(function(photo){
-	 			//Create row if counter is 0 or a modulus of 3
-	 			if(counter == 0){
-	 			 row = $('<div class="row illRow"></div>');
-	 			}
-	 			else if(counter % 3 == 0){
-	 				this.addOne(row);
-	 				row = $('<div class="row illRow"></div>');
-	 			}	
-	 			//Set up the image div 'col-md-4' located in illustrationTemplate
-	 			var photoV = new photoView({model: photo});
-	 			//add the photo div within our row
-	 			row.append(photoV.render().el);
-	 			//append the row
-	 			counter++;
-	 		}, this);
-	 		this.addOne(row);
+	 		var self = this;
+	 		var promise = new Promise(function(resolve, reject){		
+	 			self.collection.each(function(photo){
+	 				//Create row if counter is 0 or a modulus of 3
+	 				if(counter == 0){
+	 					row = $('<div class="row illRow"></div>');
+	 				}
+	 				else if(counter % 3 == 0){
+	 					resolve(row);
+	 					row = $('<div class="row illRow"></div>');
+	 				}	
+	 				//Set up the image div 'col-md-4' located in illustrationTemplate
+	 				var photoV = new photoView({model: photo});
+	 				//add the photo div within our row
+	 				row.append(photoV.render().el);
+	 				//append the row
+	 				counter++;
+	 			}, self);
+	 			resolve(row);
+	 		}).then(function(val){
+	 			$(".loading").hide();
+		 		$(".app").append(val);
+		 		$(".img-fade").animate({ opacity: 1}, 2000);
+	 		}).catch(function(error){
+	 			$(".loading").hide();
+	 			$(".app").append('<div class="error">ERROR: ' + error + '</div>');
+	 		});
 	 	},
-	 	
 	 	addOne: function(el){
 	 		//conosle.log("fuck you");
 	 		$(".loading").hide();
