552ef394af664f8c3588b3c6600382dba9fa645b
Martin <ogradymartin7@gmail.com>
2015-09-03 11:47:49 +0200

    About Page and Nav Highlights
    Made about page text requests and also changed the highlight nav
    implementation to use vanilla js rather than JQuery

--- app/public/scripts/lib/custom/helper.js
+++ app/public/scripts/lib/custom/helper.js
@@ -1,8 +1,8 @@
-define(["jquery"], function($) {
+define(function() {
 	try{
 		var url = location.href.split('/').pop();
 		if(url === 'gallery' || 'blog' || 'contact' || 'about'){
-			$('#' + url).addClass('active');
+			document.getElementById(url).setAttribute("class" , "active");
 		}
 	}catch(err){
 		//Error will happen if hit the single Illustration page from some reason
